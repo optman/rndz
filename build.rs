856ff4e5b067a7a24cf08709e79c48e2f5778c2c
optman <optman@gmail.com>
2022-05-16 15:43:07 +0800

    fix doc.rs build

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 //extern crate protobuf_codegen_pure;
 
 fn main() {
+    if std::env::var("DOCS_RS").is_ok() {
+        return;
+    }
     protobuf_codegen_pure::Codegen::new()
         .out_dir("src/proto")
         .inputs(&["proto/rndz.proto"])
